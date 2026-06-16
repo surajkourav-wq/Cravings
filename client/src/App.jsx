6ad51abd7ad12989ea37e83d7f96cbaf27311155
surajkourav-wq <surajkourav094@gmail.com>
2026-06-16 16:58:46 +0530

    feat: add Navbar route to App component for improved navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './pages/Home'
 import { BrowserRouter, Routes, Route, Router} from 'react-router-dom'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
@@ -10,7 +11,10 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/Navbar' element={<Navbar />}/>
+        <Route path='/' element={<Home />}/>
       </Routes>
+      <footer />
       </BrowserRouter>
     </>
   )
