3958130c73c001758e22a69d88a8035a5ce28e89
surajkourav-wq <surajkourav094@gmail.com>
2026-06-19 23:19:17 +0530

    feat: update Footer component with contact information and help button

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-       <div>Footer</div>
+       <div className='flex items-center justify-center'>
+        <div className='bg-red-500'>
+          Contact@client.com
+        </div>
+        <div>
+          <button className='bg-blue-500'>Get help</button>
+        </div>
+       </div>
     </>
   )
 }
